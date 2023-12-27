1ffb32b96df4a012064f38ade70088f5fa7b3c78
Rian Wahyu <rianwahyu26@gmail.com>
2023-12-27 16:28:58 +0700

    api available Time

---
+++ api/order/getAvailableTime.php
@@ -0,0 +1,35 @@
+<?php
+
+include '../../config/connection.php';
+
+$orderDate = $_POST['orderDate'];
+class data
+{
+}
+
+$query = "SELECT otm.timeReal, otm.timeDisplay
+FROM order_time_master AS otm
+WHERE NOT EXISTS (
+  SELECT b.waktuOrder
+  FROM booking AS b 
+  WHERE otm.timeReal=b.waktuOrder AND b.tanggalOrder='$orderDate') ";
+//echo $query;
+$result = mysqli_query($dbc, $query);
+if (mysqli_num_rows($result) >= 1) {
+    while ($data = mysqli_fetch_assoc($result)) {
+        $myArray[] = $data;
+    }
+
+    $response = new data();
+    $response->success = TRUE;
+    $response->message = "Berhasil Mendapatkan Data";
+    $response->data = $myArray;
+    die(json_encode($response));
+} else {
+    $response = new data();
+    $response->success = FALSE;
+    $response->message = "Tidak ada Data";
+    die(json_encode($response));
+}
+
+mysqli_close($dbc);
